44a21abe3466039b6916bd82cf02e4b0f9f52692
Maksym <maksymsydorovych@gmail.com>
2022-11-04 12:17:17 +0100

    react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import AppWithReducer from "./AppWithReducer";
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<AppWithReducer  />)
 
-ReactDOM.render(<AppWithReducer />,  document.getElementById('root'));
+// ReactDOM.render(<App />,  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
