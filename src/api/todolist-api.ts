24fd943dd9ae28024ed90780c6d2c6ee78871b5f
Maksym <maksymsydorovych@gmail.com>
2023-01-20 15:19:49 +0100

    todolist API types

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,31 +1,43 @@
 import axios from 'axios'
 
-
-const settings = {
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        // Не забываем заменить API-KEY на собственный
         'API-KEY': '4b3af02b-2c6c-49fd-b012-c48f3a9283dd'
     }
-}
+})
 
 export const todolistAPI = {
     getTodolists() {
-        return axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+        return instance.get<TodolistType[]>('todo-lists')
             .then((res) => res.data)
 
 
     },
-    createTodolists(title:string)  {
-      return  axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title}, settings)
+    createTodolists(title: string) {
+        return instance.post<ResponceType<{ item: TodolistType }>>('todo-lists', {title})
             .then((res) => res.data)
     },
-    deleteTodolist(todoId:string)  {
-        return  axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoId}`, settings)
+    deleteTodolist(todoId: string) {
+        return instance.delete<ResponceType>(`todo-lists/${todoId}`)
             .then((res) => res.data)
     },
-    updateTodolistTitle(title:string, todoId:string)  {
-        return  axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoId}`, {title}, settings)
+    updateTodolistTitle(title: string, todoId: string) {
+        return instance.put<ResponceType>(`todo-lists/${todoId}`, {title})
             .then((res) => res.data)
     }
+}
+type TodolistType = {
+    addedDate: string
+    id: string
+    order: number
+    title: string
+}
+
+type ResponceType <T={}>={
+    data: T
+    fieldsErrors: string[]
+    messages: string[]
+    resultCode: number
 }
--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -17,7 +17,7 @@ export const GetTodolists = () => {
 
     return <div> {JSON.stringify(state)}</div>
 }
-export const CreateTodolist = (title:string) => {
+export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
         let title = "REACT&REDUX"
@@ -29,7 +29,7 @@ export const CreateTodolist = (title:string) => {
 
     return <div> {JSON.stringify(state)}</div>
 }
-export const DeleteTodolist = (todoId: string) => {
+export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
         let todoId = "60a1b502-c700-41da-a9d9-476383924419"
@@ -41,7 +41,7 @@ export const DeleteTodolist = (todoId: string) => {
 
     return <div> {JSON.stringify(state)}</div>
 }
-export const UpdateTodolistTitle = (title:string, todoId:string) => {
+export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
         let todoId = "128bfb85-9ac9-40cd-bac4-26982f9f6e77"
