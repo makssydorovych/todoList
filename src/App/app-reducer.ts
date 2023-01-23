d21caa46f7d0330fce434832c6c4d360ce96811f
Maksym <maksymsydorovych@gmail.com>
2023-01-23 23:06:28 +0100

    ErrorSnackBar

--- src/App/app-reducer.ts
+++ src/App/app-reducer.ts
@@ -10,11 +10,16 @@ export const appReducer = (state: InitialStateType = initialState, action: AppAc
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, status: action.error}
         default:
             return state
     }
 }
 
 export const setStatusAc = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setErrorStatusAc = (error: RequestStatusType) => ({type: 'APP/SET-ERROR', error} as const)
 
-export type AppActionsType = ReturnType<typeof setStatusAc>
+export type AppActionsType =
+    ReturnType<typeof setStatusAc> |
+    ReturnType<typeof setErrorStatusAc>
