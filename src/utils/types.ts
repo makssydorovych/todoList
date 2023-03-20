5e83a08aba417a87311ebf898ea8faa7b4db5bb1
Maksym <maksymsydorovych@gmail.com>
2023-03-20 14:02:57 +0100

    refactor

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,11 +1,16 @@
-
 import {FieldErrorType} from '../api/types'
-import {rootReducer, store} from "../App/store";
+import {rootReducer} from "../App/store";
+import {ThunkDispatch} from "redux-thunk"
+import {AnyAction} from "redux";
+import {useDispatch} from "react-redux";
 
 
 // redux common types
 export type RootReducerType = typeof rootReducer
 // определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<RootReducerType>
-export type AppDispatchType = typeof store.dispatch
-export type ThunkError = { rejectValue: { errors: Array<string>, fieldsErrors?: Array<FieldErrorType> } }
+export type AppRootStateType = ReturnType<RootReducerType>;
+export type AppDispatchType = ThunkDispatch<AppRootStateType, any, AnyAction>;
+
+export const useAppDispatch = () => useDispatch<AppDispatchType>();
+
+export type ThunkError = { rejectValue: { errors: Array<string>, fieldsErrors?: Array<FieldErrorType> } };
